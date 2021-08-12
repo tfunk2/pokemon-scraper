3037069df2dc7706b6e3ad9ae0e403a2803ce4f5
tfunk2 <tyler.j.funk2@gmail.com>
2021-08-12 03:39:55 -0600

    Initial creation and play around session with uploader/reading HTML as string

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import pokeHTML from "./Pokemon-Shroomish-RW.gba.log.htm";
 
 function App() {
+  // const pokeString = pokeHTML
+  // const setFileToLocalStorage = (file) => {
+  //   localStorage.setItem("pokeHTML", file);
+  // };
+
+  function readSingleFile(event) {
+    //Retrieve the first (and only!) File from the FileList object
+    let pokefile = event.target.files[0];
+
+    if (pokefile) {
+      let reader = new FileReader();
+      reader.onload = (e) => {
+        var contents = e.target.result;
+        // alert(
+        //   "Got the file." +
+        //     "name: " +
+        //     pokefile.name +
+        //     ", " +
+        //     "type: " +
+        //     pokefile.type +
+        //     ", " +
+        //     "size: " +
+        //     pokefile.size +
+        //     "bytes, " +
+        //     "contents:" +
+        //     contents
+        // );
+        console.log(contents);
+        console.log("contents: ", contents.replace(/<!--.+-->/gi, ""));
+      };
+      // reader.readAsText(pokefile);
+    } else {
+      alert("Failed to load file");
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        onChange={readSingleFile}
+        id="file-picker"
+        type="file"
+        accept=".htm"
+      />
+      <iframe id="pokeHTML" title="pokemon log html" src={pokeHTML}></iframe>
     </div>
   );
 }
